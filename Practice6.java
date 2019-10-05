fb65d042eedb3a2f1832758ba9b8e6a5599a3174
kapilkumar8055 <kapil.kumar_cs18@gla.ac.in>
2019-10-05 23:36:20 +0530

    create variable and print the variable

--- Practice6.java
+++ Practice6.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Practice6 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        String string = scanner.nextLine().trim();
+        System.out.println(string.toUpperCase());
     }
 }
