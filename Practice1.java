6dac2638490127a0475a081caf2632a01b27ab67
kapilkumar8055 <kapil.kumar_cs18@gla.ac.in>
2019-10-05 22:19:01 +0530

    create two for loop for logic

--- Practice1.java
+++ Practice1.java
@@ -3,5 +3,14 @@ import java.util.Scanner;
 public class Practice1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        System.out.print("index:\t");
+        for (int i = 0; i < name.length(); i++) {
+            System.out.print(i + "\t");
+        }
+        System.out.print("\nchars:\t");
+        for (int j = 0; j < name.length(); j++) {
+            System.out.print(name.charAt(j) + "\t");
+        }
     }
 }
