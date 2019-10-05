5a4ec29f7a036c93cb79221ec2abf3d2f589722b
kapilkumar8055 <kapil.kumar_cs18@gla.ac.in>
2019-10-05 22:48:05 +0530

    create variable inputstring and index

--- Practice2.java
+++ Practice2.java
@@ -3,6 +3,11 @@ import java.util.Scanner;
 public class Practice2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter String");
+        String inputString = scanner.nextLine();
+        System.out.println("Enter index of substring");
+        int index = scanner.nextInt();
+        System.out.println("Substring is");
 
     }
 }
